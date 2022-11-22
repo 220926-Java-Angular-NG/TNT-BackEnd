153312a7bc673d29c794f5d8024fd5b579658ee2
NedTurner613 <nedturner613@gmail.com>
2022-11-22 11:59:25 -0600

    added user service testing class

---
+++ src/test/java/com/revature/services/UserServiceTesting.java
@@ -0,0 +1,72 @@
+package com.revature.services;
+
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class UserServiceTesting {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private User newUser;
+    private User dbUser;
+    private Optional<User> optDbUser;
+
+    private String email = "bigboy@sally.forth";
+    private String password = "password";
+    private String firstName = "Grimbo";
+    private String lastName = "McFulstrom";
+
+
+    @BeforeEach
+    public void populateUserObjects(){
+        newUser = new User();
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        newUser.setFirstName(firstName);
+        newUser.setLastName(lastName);
+
+
+        dbUser = new User();
+        dbUser.setId(1);
+        dbUser.setEmail(email);
+        dbUser.setPassword(password);
+        dbUser.setFirstName(firstName);
+        dbUser.setLastName(lastName);
+        optDbUser = Optional.ofNullable(dbUser);
+    }
+
+
+    @Test
+    public void givenEmailAndPassword_findByCredentials_returnsOptionalUser(){
+        Mockito.when(userRepository.findByEmailAndPassword(email, password)).thenReturn(optDbUser);
+
+        Optional<User> returnedUser = userService.findByCredentials(email, password);
+
+        Assertions.assertEquals(optDbUser.get().getId(), returnedUser.get().getId());
+        Assertions.assertEquals(optDbUser.get().getEmail(), returnedUser.get().getEmail());
+        Assertions.assertEquals(optDbUser.get().getPassword(), returnedUser.get().getPassword());
+        Assertions.assertEquals(optDbUser.get().getFirstName(), returnedUser.get().getFirstName());
+        Assertions.assertEquals(optDbUser.get().getLastName(), returnedUser.get().getLastName());
+
+    }
+
+
+
+
+
+
+}
